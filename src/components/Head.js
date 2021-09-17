fd919fd2c70ec2ce51b70656ee7cb816c320f665
ChrisColley <chris.s.colley@gmail.com>
2021-09-18 08:23:11 +1000

    Changed og:image from png to jpg

--- src/components/Head.js
+++ src/components/Head.js
@@ -8,7 +8,10 @@ const Head = () => (
       property="og:description"
       content="Exciting content coming soon! Currently under construction"
     />
-    <meta property="og:image" content="../images/og-image1.png" />
+    <meta property="og:image" content="../images/og-image1.jpg" />
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta property="og:image:width" content="400" />
+    <meta property="og:image:height" content="300" />
     <meta property="og:url" content="https://www.chriscolley.com.au" />
     <meta property="og:type" content="website" />
   </Helmet>
