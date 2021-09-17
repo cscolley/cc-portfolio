0e883c2543344b18d2f48cecec32938056e2c81f
ChrisColley <chris.s.colley@gmail.com>
2021-09-17 18:00:18 +1000

    updated icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        icon: "src/images/cclogo50x26.png",
       },
     },
     "gatsby-transformer-remark",
